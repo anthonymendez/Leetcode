ea7bb18c25f208248d15dd31e1d0dca9b23337bc
tonydetiger <anthonymendez9@gmail.com>
2017-03-22 20:57:40 -0400

    Re-did Problem #371 with Bit Manipulation

--- src/Problem371.java
+++ src/Problem371.java
@@ -1,9 +1,23 @@
 import java.math.BigInteger;
 
 public class Problem371 {
+//	public int getSum(int a, int b) {
+//		BigInteger bi = BigInteger.valueOf(a);
+//		bi = bi.add(BigInteger.valueOf(b));
+//		return bi.intValue();
+//	}
+	// Bit Manipulation
+	// Yup, using BigInteger was definitely cheating
 	public int getSum(int a, int b) {
-		BigInteger bi = BigInteger.valueOf(a);
-		bi = bi.add(BigInteger.valueOf(b));
-		return bi.intValue();
+		if(a == 0) return b;
+		else if(b == 0) return a;
+		
+		while(b != 0){
+			int leftOver = a & b;
+			a = a ^ b;
+			b = leftOver << 1;
+		}
+		
+		return a;		
 	}
 }
