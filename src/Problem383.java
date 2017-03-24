644bc07aa5f5ff008bfb8b56c1db3f0ec80f4fa2
tonydetiger <anthonymendez9@gmail.com>
2017-03-24 17:52:27 -0400

    Solve problem #383

---
+++ src/Problem383.java
@@ -0,0 +1,15 @@
+
+public class Problem383 {
+	public boolean canConstruct(String ransomNote, String magazine) {
+		int[] magAmt = new int[128], noteAmt = new int[128];
+		for(char c : ransomNote.toCharArray())
+			noteAmt[c]++;
+		for(char c : magazine.toCharArray())
+			magAmt[c]++;
+		for(int i = 0; i < 128; i++){
+			if(magAmt[i] < noteAmt[i])
+				return false
+		}
+		return true;
+	}
+}
